7c996b4c1d94606f2d68cf0f2e7b911b5827018b
Gabriel Borges <140124714+devBorges14@users.noreply.github.com>
2025-10-27 14:15:24 -0300

    Criação da classe e uso de metodos simples

---
+++ Main.java
@@ -0,0 +1,64 @@
+package UserProject;
+
+import java.util.Scanner;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+class user {
+    String name;
+    String email;
+    String password;
+    boolean premium;
+
+    public String premiumUser(boolean premium) {
+        if (premium) {
+            return "Usuário Premium";
+        } else {
+            return "Usuário Comum";
+        }
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanne = new Scanner(System.in);
+        user u1 = new user();
+        {
+            u1.premium = false;
+            System.out.print("Digite seu nome: ");
+            u1.name = scanne.nextLine(); // lê e armazena no atributo name
+
+            System.out.print("Digite seu email: ");
+            u1.email = scanne.nextLine(); // lê e armazena no atributo email
+
+            System.out.print("Digite seu password: ");
+            u1.password = scanne.nextLine(); // lê e armazena no atributo password
+        } // User 1
+
+         user u2 = new user();
+        {
+            u2.premium = true;
+            System.out.print("Digite seu nome: ");
+            u2.name = scanne.nextLine(); // lê e armazena no atributo name
+
+            System.out.print("Digite seu email: ");
+            u2.email = scanne.nextLine(); // lê e armazena no atributo email
+
+            System.out.print("Digite seu password: ");
+            u2.password = scanne.nextLine(); // lê e armazena no atributo password
+        } // User 2
+
+        Dictionary<String, String> usersDic = new Hashtable<>();
+
+        // put(key, value)
+        usersDic.put(u1.email, "name: " + u1.name + ", email: " + u1.email + ", password: " + u1.password + ", premium?: " + u1.premium );
+        System.out.println(usersDic);
+        
+        // put(key, value)
+        usersDic.put(u2.email, "name: " + u2.name + ", email: " + u2.email + ", password: " + u2.password + ", premium?: " + u2.premium );
+        System.out.println(usersDic);
+
+        scanne.close();
+    }
+}
