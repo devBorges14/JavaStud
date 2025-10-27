0bb760cbeff8eb9810b5232065ca7069f44b9b8a
Gabriel Borges <140124714+devBorges14@users.noreply.github.com>
2025-10-27 15:03:06 -0300

    Criei um sistema de login bem simples

--- Main.java
+++ Main.java
@@ -36,29 +36,47 @@ public class Main {
             u1.password = scanne.nextLine(); // lê e armazena no atributo password
         } // User 1
 
-         user u2 = new user();
-        {
-            u2.premium = true;
-            System.out.print("Digite seu nome: ");
-            u2.name = scanne.nextLine(); // lê e armazena no atributo name
+        //  user u2 = new user();
+        // {
+        //     u2.premium = true;
+        //     System.out.print("Digite seu nome: ");
+        //     u2.name = scanne.nextLine(); // lê e armazena no atributo name
 
-            System.out.print("Digite seu email: ");
-            u2.email = scanne.nextLine(); // lê e armazena no atributo email
+        //     System.out.print("Digite seu email: ");
+        //     u2.email = scanne.nextLine(); // lê e armazena no atributo email
 
-            System.out.print("Digite seu password: ");
-            u2.password = scanne.nextLine(); // lê e armazena no atributo password
-        } // User 2
+        //     System.out.print("Digite seu password: ");
+        //     u2.password = scanne.nextLine(); // lê e armazena no atributo password
+        // } // User 2
 
         Dictionary<String, String> usersDic = new Hashtable<>();
+        usersDic.put(u1.email, "name: " + u1.name + ", email: " + u1.email + ", password: " + u1.password + ", premium?: " + u1.premium ); // User 1
 
-        // put(key, value)
-        usersDic.put(u1.email, "name: " + u1.name + ", email: " + u1.email + ", password: " + u1.password + ", premium?: " + u1.premium );
-        System.out.println(usersDic);
         
-        // put(key, value)
-        usersDic.put(u2.email, "name: " + u2.name + ", email: " + u2.email + ", password: " + u2.password + ", premium?: " + u2.premium );
-        System.out.println(usersDic);
+        // // put(key, value)
+        // usersDic.put(u2.email, "name: " + u2.name + ", email: " + u2.email + ", password: " + u2.password + ", premium?: " + u2.premium );
+        // System.out.println(usersDic);
+        
+        
+        Scanner input = new Scanner(System.in);
 
+         System.out.println("----------------------------| LOGIN |----------------------");
+        System.out.print("Email: ");
+        String u = input.nextLine();
+        
+        System.out.print("Senha: ");
+        String s = input.nextLine();
+        System.out.println("------------------------------------------------------------");
+        if (u.equals(u1.email) && s.equals(u1.password)) {
+            System.out.println("Login bem-sucedido!");
+            // put(key, value)
+            System.out.println(usersDic);
+        } else {
+            System.out.println("Usuário ou senha incorretos.");
+        }
+        
+        
         scanne.close();
+        input.close();
     }
 }
